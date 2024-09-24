b8836aec2ecdd3fab1748da6e6eb44b6cda15a7b
appleaww <artemmorozz2006@gmail.com>
2024-09-24 15:48:17 +0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -3,9 +3,16 @@ using namespace std;
 int main() {
     int a;
     int b;
+    int m;
     std::cout <<"Введите число a\n";
     std::cin>> a;
     std::cout <<"Введите число b\n";
     std::cin>> b;
+
+
+    std::cout <<"Введите число m\n";
+    std::cin>> m;
+    std::cout <<"Сумма чисел a и b равна " << a+b << "\n";
+
     return 0;
 }
