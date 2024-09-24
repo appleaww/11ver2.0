a9f82f71db0d8b00d26b7aa862fcd480e27491c0
appleaww <artemmorozz2006@gmail.com>
2024-09-24 15:45:50 +0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -3,22 +3,9 @@ using namespace std;
 int main() {
     int a;
     int b;
-    int o;
-    int p;
-    int m;
     std::cout <<"Введите число a\n";
     std::cin>> a;
     std::cout <<"Введите число b\n";
     std::cin>> b;
-    std::cout <<"Введите число o\n";
-    std::cin>> o;
-    std::cout <<"Введите число p\n";
-    std::cin>> p;
-    std::cout<< "Произведение чисел o и p равно " << o *p<< "\n";
-    std::cout <<"Введите число m\n";
-    std::cin>> m;
-    std::cout <<"Сумма чисел m и a равна " << m+a << "\n";
-    std::cout <<"Сумма чисел m и b равна " << m+b << "\n";
-
     return 0;
 }
