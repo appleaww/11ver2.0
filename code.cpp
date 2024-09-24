934432ad37c92ca1f1085e0272538b5648740a7a
appleaww <artemmorozz2006@gmail.com>
2024-09-24 15:50:09 +0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -5,6 +5,7 @@ int main() {
     int b;
     int o;
     int p;
+    int m;
     std::cout <<"Введите число a\n";
     std::cin>> a;
     std::cout <<"Введите число b\n";
@@ -14,5 +15,9 @@ int main() {
     std::cout <<"Введите число p\n";
     std::cin>> p;
     std::cout<< "Произведение чисел o и p равно " << o *p<< "\n";
+    std::cout <<"Введите число m\n";
+    std::cin>> m;
+    std::cout <<"Сумма чисел m и a равна " << m+a << "\n";
+ 
     return 0;
 }
