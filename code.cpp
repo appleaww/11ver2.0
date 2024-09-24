026a3adf73a7f066e73e765d9afb2d3973ae77a1
appleaww <artemmorozz2006@gmail.com>
2024-09-24 15:52:34 +0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -3,9 +3,15 @@ using namespace std;
 int main() {
     int a;
     int b;
+    int o;
+    int p;
     std::cout <<"Введите число a\n";
     std::cin>> a;
     std::cout <<"Введите число b\n";
     std::cin>> b;
+    std::cout <<"Введите число o\n";
+    std::cin>> o;
+    std::cout <<"Введите число p\n";
+    std::cin>> p;
     return 0;
 }
