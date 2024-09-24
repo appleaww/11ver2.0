bfeb3429b1da80f021bbbeb4cb0d78799b08b7cb
appleaww <artemmorozz2006@gmail.com>
2024-09-24 15:42:56 +0300

    Update code.cpp

---
+++ code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    int b;
+    std::cout <<"Введите число a\n";
+    std::cin>> a;
+    std::cout <<"Введите число b\n";
+    std::cin>> b;
+    return 0;
+}
